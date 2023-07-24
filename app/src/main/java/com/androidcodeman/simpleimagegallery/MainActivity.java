81e70e48e78b0e87b8639741bd844e7da88b7ed8
Hudaif Ahamed <hudaifahamed08@gmail.com>
2023-07-24 20:47:50 +0530

    Initial commit

---
+++ app/src/main/java/com/androidcodeman/simpleimagegallery/MainActivity.java
@@ -0,0 +1,257 @@
+package com.androidcodeman.simpleimagegallery;
+
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.androidcodeman.simpleimagegallery.utils.MarginDecoration;
+import com.androidcodeman.simpleimagegallery.utils.PicHolder;
+import com.androidcodeman.simpleimagegallery.utils.imageFolder;
+import com.androidcodeman.simpleimagegallery.utils.itemClickListener;
+import com.androidcodeman.simpleimagegallery.utils.pictureFacer;
+import com.androidcodeman.simpleimagegallery.utils.pictureFolderAdapter;
+
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity implements itemClickListener {
+    Button cameraButton;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String IMAGE_FILE_NAME = "captured_image.jpg";
+    RecyclerView folderRecycler;
+    TextView empty;
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+
+    /**
+     * Request the user for permission to access media files and read images on the device
+     * this will be useful as from api 21 and above, if this check is not done the Activity will crash
+     *
+     * Setting up the RecyclerView and getting all folders that contain pictures from the device
+     * the getPicturePaths() returns an ArrayList of imageFolder objects that is then used to
+     * create a RecyclerView Adapter that is set to the RecyclerViewā
+     *
+     * @param savedInstanceState saving the activity state
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraButton=findViewById(R.id.cameraButton);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+//                Intent intent = new Intent(MainActivity.this,Capture.class);
+//                startActivity(intent);
+            }
+        });
+
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        //____________________________________________________________________________________
+
+        empty =findViewById(R.id.empty);
+
+        folderRecycler = findViewById(R.id.folderRecycler);
+        folderRecycler.addItemDecoration(new MarginDecoration(this));
+        folderRecycler.hasFixedSize();
+        ArrayList<imageFolder> folds = getPicturePaths();
+
+        if(folds.isEmpty()){
+            empty.setVisibility(View.VISIBLE);
+        }else{
+            RecyclerView.Adapter<pictureFolderAdapter.FolderHolder> folderAdapter = new pictureFolderAdapter(folds,MainActivity.this,this);
+            folderRecycler.setAdapter(folderAdapter);
+        }
+
+        changeStatusBarColor();
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = null;
+            if (data != null) {
+                extras = data.getExtras();
+            }
+            if (extras != null) {
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                if (imageBitmap != null) {
+                    saveImageToGallery(imageBitmap);
+
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void saveImageToGallery(Bitmap bitmap) {
+        ContentResolver resolver = getContentResolver();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, IMAGE_FILE_NAME);
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+
+        getPicturePaths();
+
+        try {
+            OutputStream outputStream = null;
+            if (imageUri != null) {
+                outputStream = resolver.openOutputStream(imageUri);
+            }
+            if (outputStream != null) {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                outputStream.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    /**1
+     * @return
+     * gets all folders with pictures on the device and loads each of them in a custom object imageFolder
+     * the returns an ArrayList of these custom objects
+     */
+    private ArrayList<imageFolder> getPicturePaths()
+    {
+        ArrayList<imageFolder> picFolders = new ArrayList<>();
+        ArrayList<String> picPaths = new ArrayList<>();
+        Uri allImagesuri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = { MediaStore.Images.ImageColumns.DATA ,MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME,MediaStore.Images.Media.BUCKET_ID};
+        Cursor cursor = this.getContentResolver().query(allImagesuri, projection, null, null, null);
+        try
+        {
+
+            if (cursor != null)
+            {
+                cursor.moveToFirst();
+            }
+            do
+            {
+                imageFolder folds = new imageFolder();
+               // String name = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME));
+                String folder = cursor != null ? cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME)) : null;
+                assert cursor != null;
+                String datapath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
+
+                //String folderpaths =  datapath.replace(name,"");
+                String folderpaths = datapath.substring(0, datapath.lastIndexOf(folder+"/"));
+                folderpaths = folderpaths+folder+"/";
+                if (!picPaths.contains(folderpaths)) {
+                    picPaths.add(folderpaths);
+
+                    folds.setPath(folderpaths);
+                    folds.setFolderName(folder);
+                    folds.setFirstPic(datapath);//if the folder has only one picture this line helps to set it as first so as to avoid blank image in itemview
+                    folds.addpics();
+                    picFolders.add(folds);
+                }else{
+                    for(int i = 0;i<picFolders.size();i++){
+                        if(picFolders.get(i).getPath().equals(folderpaths)){
+                            picFolders.get(i).setFirstPic(datapath);
+                            picFolders.get(i).addpics();
+                        }
+                    }
+                }
+            }
+            while(cursor.moveToNext());
+            cursor.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        for(int i = 0;i < picFolders.size();i++){
+            Log.d("picture folders",picFolders.get(i).getFolderName()+" and path = "+picFolders.get(i).getPath()+" "+picFolders.get(i).getNumberOfPics());
+        }
+
+        //reverse order ArrayList
+       /* ArrayList<imageFolder> reverseFolders = new ArrayList<>();
+
+        for(int i = picFolders.s
+        ize()-1;i > reverseFolders.size()-1;i--){
+            reverseFolders.add(picFolders.get(i));
+        }*/
+
+        return picFolders;
+    }
+
+
+    @Override
+    public void onPicClicked(PicHolder holder, int position, ArrayList<pictureFacer> pics) {
+
+    }
+
+    /**
+     * Each time an item in the RecyclerView is clicked this method from the implementation of the transitListerner
+     * in this activity is executed, this is possible because this class is passed as a parameter in the creation
+     * of the RecyclerView's Adapter, see the adapter class to understand better what is happening here
+     * @param pictureFolderPath a String corresponding to a folder path on the device external storage
+     */
+    @Override
+    public void onPicClicked(String pictureFolderPath,String folderName) {
+        Intent move = new Intent(MainActivity.this,ImageDisplay.class);
+        move.putExtra("folderPath",pictureFolderPath);
+        move.putExtra("folderName",folderName);
+
+        //move.putExtra("recyclerItemSize",getCardsOptimalWidth(4));
+        startActivity(move);
+    }
+
+
+    /**
+     * Default status bar height 24dp,with code API level 24
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void changeStatusBarColor()
+    {
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(getApplicationContext(),R.color.black));
+
+    }
+
+}
